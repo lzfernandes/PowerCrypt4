0291f0836a084f5906a0f450c9801eedb5e41215
0xFireball <0xFireball@outlook.com>
2016-03-26 11:08:03 -0700

    update version info

--- PowerCrypt4/Properties/AssemblyInfo.cs
+++ PowerCrypt4/Properties/AssemblyInfo.cs
@@ -1,31 +1,29 @@
-﻿#region Using directives
-
+#region Using directives
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 #endregion
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PowerCrypt4")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PowerCrypt4")]
-[assembly: AssemblyCopyright("Copyright 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyTitle ("PowerCrypt4")]
+[assembly: AssemblyDescription ("PowerAES and PowerRSA simple cryptography libraries. Now includes advanced Symmetric Cryptography, ByteConverter, and SHA-256, SHA-384, and SHA-512.")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("OmniBean, Inc.")]
+[assembly: AssemblyProduct ("PowerCrypt4")]
+[assembly: AssemblyCopyright ("Copyright OmniBean, Inc., 2015-2016")]
+[assembly: AssemblyTrademark ("PowerCrypt4")]
+[assembly: AssemblyCulture ("")]
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
-[assembly: ComVisible(false)]
-
+[assembly: ComVisible (false)]
 // The assembly version has following format :
 //
 // Major.Minor.Build.Revision
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ("4.3.0.0")]
+[assembly: AssemblyFileVersion ("4.3.0.0")]
+[assembly: Guid ("1c72adc2-e59a-45e3-974f-8b5f42842270")]
