910ec722d4038678f2f2a6ba6c0d228e22ac40e3
0xFireball <0xFireball@outlook.com>
2016-04-23 22:17:35 -0700

    add more rsa extensions

--- PowerCrypt4/RSAExtensions.cs
+++ PowerCrypt4/RSAExtensions.cs
@@ -3,8 +3,13 @@ using System.IO;
 using System.Text;
 using OmniBean.PowerCrypt4.Utilities;
 using System.Security.Cryptography;
-using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Cms;
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Utilities.Encoders;
 
 namespace OmniBean.PowerCrypt4
 {
@@ -35,6 +40,40 @@ namespace OmniBean.PowerCrypt4
 				outputPem = sw.ToString();
             }
 			return outputPem;
+        }
+        
+        public static string SignWithPKCSPrivateKey(string plaintext, PowerRSA prsa)
+        {
+			var signer = SignerUtilities.GetSigner("SHA256withRSA");
+			var rsa = RSA.Create();
+			rsa.FromXmlString(prsa.PrivateKey);
+            var bcKeyPair = DotNetUtilities.GetRsaKeyPair(rsa);
+			signer.Init(true, bcKeyPair.Private);
+			byte[] byteplaintext = Encoding.UTF8.GetBytes(plaintext);
+			signer.BlockUpdate(byteplaintext, 0, byteplaintext.Length);
+			byte[] signature = signer.GenerateSignature();
+            /* Base 64 encode the sig so its 8-bit clean */
+            var signedString = Convert.ToBase64String(signature);
+            return signedString;
+        }
+        
+        public static bool VerifyWithPKCSPublicKey(String message, String expectedSignature, PowerRSA prsa)
+        {
+        	var rsa = RSA.Create();
+			rsa.FromXmlString(prsa.PublicKey);
+			/* Make the key */
+			RsaKeyParameters key = DotNetUtilities.GetRsaPublicKey(rsa);
+            /* Init alg */
+            var signer = SignerUtilities.GetSigner("SHA256withRSA");
+            /* Populate key */
+            signer.Init(false, key);
+            /* Get the signature into bytes */
+            var expectedSig = Convert.FromBase64String(expectedSignature);
+            /* Get the bytes to be signed from the string */
+            var msgBytes = Encoding.UTF8.GetBytes(message);
+            /* Calculate the signature and see if it matches */
+            signer.BlockUpdate(msgBytes, 0, msgBytes.Length);
+            return signer.VerifySignature(expectedSig);
         }
     }
 }
