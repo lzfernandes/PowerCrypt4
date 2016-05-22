9c2639caf3652d81842129c930c5282f15d8ce03
0xFireball <0xFireball@outlook.com>
2016-05-22 11:04:54 -0700

    remove unused refs

--- PowerCrypt4/ByteConverter.cs
+++ PowerCrypt4/ByteConverter.cs
@@ -22,14 +22,14 @@ namespace OmniBean.PowerCrypt4.Utilities
 
         public static byte[] RawGetBytes(this string str)
         {
-            var bytes = new byte[str.Length*sizeof(char)];
+            var bytes = new byte[str.Length * sizeof(char)];
             Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
             return bytes;
         }
 
         public static string RawGetString(this byte[] bytes)
         {
-            var chars = new char[bytes.Length/sizeof(char)];
+            var chars = new char[bytes.Length / sizeof(char)];
             Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
             return new string(chars);
         }
